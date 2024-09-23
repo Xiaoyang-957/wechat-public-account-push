98db40b464a6e46cc2bbb51a359a62a48ddcdf73
Xiaoyang-957 <2373986017@qq.com>
2024-09-23 12:37:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oD8HQ6UNJ6udTa1c9P8JP54DSh-k',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'MgutJYWRPcMM8G_rPjUZOLOFO8o6S35xycnWT5EV5NY	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'MgutJYWRPcMM8G_rPjUZOLOFO8o6S35xycnWT5EV5NY',
+  CALLBACK_TEMPLATE_ID: '39Q3Dq6_xNtihc8VN4AZEpTlV_yt0CglBpQvOfV4zt4',
 
   CALLBACK_USERS: [
     {
